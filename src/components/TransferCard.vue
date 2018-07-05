287dc970a4898a3d52b2988b4682a9db36fadaf7
Arel <arel.hello@gmail.com>
2018-07-05 17:51:55 +0800

    Add Transfer Card UI

--- src/components/TransferCard.vue
+++ src/components/TransferCard.vue
@@ -1,5 +1,39 @@
 <template>
-<div></div>
+<div>
+  <h4>我的七彩寶石</h4>
+  <p><span class="wallet-value">{{currentGem}}</span>點</p>
+  <Form ref="transferCard" :model="transferCard" :rules="transferCardRule" label-position="top" style="max-width:300px">
+    <FormItem label="錢包位置" prop="walletAddress">
+      <Input type="text" readonly v-model="transferCard.walletAddress"></Input>
+    </FormItem>
+    <FormItem :label="`兌換比例: ${transferRate}`">
+      <Row>
+        <Col span="11">
+          <FormItem prop="fromValue">
+            <InputNumber :min="0" v-model="fromValue"></InputNumber>
+            <span>七彩寶石</span>
+          </FormItem>
+        </Col>
+        <Col span="2">-></Col>
+        <Col span="11">
+          <FormItem prop="toValue">
+            <InputNumber :min="0" v-model="toValue"></InputNumber>
+            <span>卡片</span>
+          </FormItem>
+        </Col>
+      </Row>
+    </FormItem>
+    <FormItem label="請輸入二級密碼" prop="password">
+      <Input type="password" v-model="transferCard.password"></Input>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="handleSubmit('transferCard')">申請轉換</Button>
+    </FormItem>
+  </Form>
+  <h4>寶石轉換申請狀態</h4>
+<!--   <Page :total="paging.total" :page-size="paging.pre_page" simple size="small" @on-change="changePage($event)"></Page> -->
+   <Table :columns="transferApplyCols" :data="transferCardList"></Table>
+</div>
 </template>
 
 <script>
@@ -8,12 +42,118 @@ export default {
   name: 'ApplyCard',
 
   data () {
-    return {
-
+    const validatePass = (rule, value, callback) => {
+      if (value === '' || value.length < 6) {
+        callback(new Error('填入密碼，符合長度 6 個字元以上'))
+      }
+      callback()
     }
+    const validateFromValue = (rule, value, callback) => {
+      if (value <= 0) {
+        callback(new Error('轉換數量必須大於 0'))
+      }
+      if (value % this.fromLimit !== 0) {
+        callback(new Error(`轉換數量必須是 ${this.fromLimit} 的倍數`))
+      }
+      callback()
+    }
+    return {
+      transferCard: {
+        walletAddress: '',
+        fromValue: 0,
+        toValue: 0,
+        password: '',
+      },
+      transferCardRule: {
+        fromValue: [
+          { required: true, type: 'number', validator: validateFromValue, trigger: 'blur' },
+        ],
+        password: [
+          { required: true, validator: validatePass, trigger: 'blur' },
+        ],
+      },
+      transferApplyCols: [
+        {
+          title: '申請編號',
+          key: 'to_gem',
+        },
+        {
+          title: '匯率',
+          key: 'rate',
+        },
+        {
+          title: '數量',
+          key: 'amount',
+        },
+        {
+          title: '目前狀態',
+          key: 'status',
+        },
+        {
+          title: '建立時間',
+          key: 'updated_at',
+        },
+      ],
+    }
+  },
+  computed: {
+    // current gem
+    currentGem () {
+      return this.$store.getters.wallet.find(wallet => wallet.gem === 0).amount
+    },
+    transferRate () {
+      return '1:1'
+    },
+    fromValue: {
+      get () {
+        return this.transferCard.fromValue
+      },
+      set (value) {
+        this.transferCard.fromValue = Number(value)
+        this.transferCard.toValue = Number((value * this.toFromRatio).toFixed(1))
+      },
+    },
+    toValue: {
+      get () {
+        return this.transferCard.toValue
+      },
+      set (value) {
+        this.transferCard.toValue = Number(value)
+        this.transferCard.fromValue = Number((value / this.toFromRatio).toFixed(1))
+      },
+    },
+    fromLimit () {
+      return this.transferRate.charAt(0)
+    },
+    toLimit () {
+      return this.transferRate.charAt(2)
+    },
+    toFromRatio () {
+      return this.toLimit / this.fromLimit
+    },
+    transferCardList () {
+      return []
+    },
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('轉出成功!')
+        } else {
+          this.$Message.error('請確實填寫資料!')
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    },
   },
 }
 </script>
 
 <style lang="css">
+.wallet-value {
+  font-size: 2em;
+}
 </style>
